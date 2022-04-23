bee4f80d42fcd780a5c7596d6a9a6f5d941019cf
Sneruall <laurens@vr-house.nl>
2022-04-23 17:26:36 +0200

    reverse the mapping so sorting joblist is with latest first

--- components/JobListing.tsx
+++ components/JobListing.tsx
@@ -3,11 +3,13 @@ import { Job } from '../types/types';
 import JobItem from './JobItem';
 
 const JobListing: React.FC<{ jobs: [Job] }> = ({ jobs }) => {
-  const joblist = jobs.map((job) => (
-    <li className="list-none" key={job._id}>
-      <JobItem job={job} />
-    </li>
-  ));
+  const joblist = jobs
+    .map((job) => (
+      <li className="list-none" key={job._id}>
+        <JobItem job={job} />
+      </li>
+    ))
+    .reverse();
 
   return <div className="flex flex-col gap-3">{joblist}</div>;
 };

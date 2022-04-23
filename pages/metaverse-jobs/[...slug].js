eb8a287792cad35e312b47be0854df7c3a1f4577
Sneruall <laurens@vr-house.nl>
2022-04-23 18:01:07 +0200

    Retrieving database data based on queried id and matched with db

--- pages/metaverse-jobs/[...slug].js
+++ pages/metaverse-jobs/[...slug].js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import clientPromise from '../../lib/mongodb';
 
 /* 
 - Create urls like: www.metaversed.careers/metaverse-jobs/123456-remote-senior-ux-ui-designer-ez360
@@ -11,6 +12,12 @@ OR DO IT LIKE THIS: /metaverse-jobs/$tag1/$jobtitle (e.g. design/jobtitle)
 and then use /metaverse-jobs/$tag1 also as job-list page for specific category.
 to do this, simply add all tag1 categories as folders under /metaverse-jobs and
 add a ...slug under each of them for the job detail pages.
+*/
+
+/*
+TODO:
+- Handle if id is not found in db (forward to 404 page)
+- Import all necessary data to render the page..
 */
 
 const JobPage = (props) => {
@@ -27,7 +34,7 @@ export default JobPage;
 
 export async function getServerSideProps(context) {
   const { slug } = context.query;
-  const id = /^[^-]*/.exec(context.query.slug[0])[0]; //removes everything after the first minus sign (to get the id)
+  const queryId = /^[^-]*/.exec(context.query.slug[0])[0]; //removes everything after the first minus sign (to get the id)
 
   // make a call the the database and load the data to render the page in SSR
 
@@ -35,5 +42,14 @@ export async function getServerSideProps(context) {
   // const country = await res.json();
 
   // console.log(`Fetched place: ${country.name}`);
-  return { props: { id } };
+
+  const client = await clientPromise;
+
+  const db = client.db();
+  const job = await db.collection('metaverseJobs').findOne({ id: queryId });
+  // .sort({ metacritic: -1 })
+  // .limit(20)
+  // .toArray();
+
+  return { props: { id: JSON.stringify(job.id) } }; //if you stringify(job) instead of job.id you get the full data, todo: import all data and make it usable in the page level.
 }

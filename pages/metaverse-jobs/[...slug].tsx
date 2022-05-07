8bef765b9472df2eea5d5a19e2a8fe51292d34c6
Sneruall <laurens@vr-house.nl>
2022-05-07 20:36:23 +0200

    style job page with header

--- pages/metaverse-jobs/[...slug].tsx
+++ pages/metaverse-jobs/[...slug].tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from 'next';
 import { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../../components/Header';
 import clientPromise from '../../lib/mongodb';
 import { Job } from '../../types/types';
 
@@ -10,6 +12,12 @@ interface Props {
 const JobPage: NextPage<Props> = (props) => {
   return (
     <div>
+      <Head>
+        <title>Metaversed Careers</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
       <p>Job id: {props.data.id}</p>
       <p>Job title: {props.data.jobTitle}</p>
       <p>Job location: {props.data.location}</p>

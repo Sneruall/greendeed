beff721b8ad7dc6e88e0ce81fab0fc32c484ff5e
Sneruall <laurens@vr-house.nl>
2022-07-01 08:19:10 +0200

    filter for jobs based on array of objects (matched ids)

--- backend/job/remotive/apiCall.ts
+++ backend/job/remotive/apiCall.ts
@@ -3,11 +3,23 @@ import { mapRemotiveJobtoJob } from './jobMapper';
 
 export async function getRemotiveJobs() {
   // Fetch data from external API
-  const res = await fetch(`https://remotive.com/api/remote-jobs?limit=20`);
+  const res = await fetch(`https://remotive.com/api/remote-jobs`);
 
   const data = await res.json();
 
-  const filteredData = data.jobs.filter((x) => x.id === 1294241);
+  // const filteredData = data.jobs.filter((x) => x.id === 1294241);
+
+  // console.log(filteredData);
+
+  const myFilter = [
+    { id: 1294241, sdg: [1, 2, 3] },
+    { id: 1275809, sdg: [1, 2, 3] },
+  ];
+  const filteredData = data.jobs.filter((el) => {
+    return myFilter.some((f) => {
+      return f.id === el.id;
+    });
+  });
 
   console.log(filteredData);
 

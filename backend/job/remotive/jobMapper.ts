626ad27eda2a275afe41c60952b7e6748d355e6e
Sneruall <laurens@vr-house.nl>
2022-07-16 10:54:56 +0200

    remotive job category mapper

--- backend/job/remotive/jobMapper.ts
+++ backend/job/remotive/jobMapper.ts
@@ -1,13 +1,13 @@
-import { jobCategoriesList } from '../../../types/jobCategories';
+import { jobCategoriesList, jobCategory } from '../../../types/jobCategories';
 import { Job, remotiveJob } from '../../../types/types';
 
 export const mapRemotiveJobtoJob = (remotiveJob: remotiveJob): Job => {
   const job: Job = {
     companyName: remotiveJob.company_name,
-    companyId: Math.random().toString(), //todo fixen
-    companyDescription: '',
+    companyId: 'external',
+    companyDescription: 'external',
     jobTitle: remotiveJob.title,
-    category: jobCategoriesList[4], //todo categories mappen
+    category: mapRemotiveCategorytoJobCategory(remotiveJob.category),
     tags: remotiveJob.tags,
     jobDescription: remotiveJob.description,
     jobType: 'Other', //todo job_type naar jobType mappen
@@ -30,4 +30,22 @@ export const mapRemotiveJobtoJob = (remotiveJob: remotiveJob): Job => {
     external: true,
   };
   return job;
+};
+
+export const mapRemotiveCategorytoJobCategory = (
+  remotiveJobCategory: string
+): jobCategory => {
+  // Remotive uses QA instead of Quality Assurance
+  if (remotiveJobCategory === 'QA') {
+    remotiveJobCategory = 'Quality Assurance';
+  }
+
+  // Check the incoming cateogry from remotive and match it with the jobCategoriesList
+  const category = jobCategoriesList.find(
+    (category) => category.name === remotiveJobCategory
+  );
+  if (!category) {
+    return jobCategoriesList.find((category) => category.name === 'Other')!;
+  }
+  return category;
 };
--- types/jobCategories.ts
+++ types/jobCategories.ts
@@ -13,18 +13,18 @@ export const generateCategoriesArray = () => {
 export const jobCategoriesList: jobCategory[] = [
   {
     id: 1,
-    name: 'IT',
-    slug: 'it',
+    name: 'Software Development',
+    slug: 'software-development',
   },
   {
     id: 2,
-    name: 'Operations',
-    slug: 'operations',
+    name: 'Customer Service',
+    slug: 'customer-service',
   },
   {
     id: 3,
-    name: 'Manufacturing',
-    slug: 'manufacturing',
+    name: 'Design',
+    slug: 'design',
   },
   {
     id: 3,
@@ -33,6 +33,56 @@ export const jobCategoriesList: jobCategory[] = [
   },
   {
     id: 4,
+    name: 'Marketing',
+    slug: 'marketing',
+  },
+  {
+    id: 5,
+    name: 'Sales',
+    slug: 'sales',
+  },
+  {
+    id: 6,
+    name: 'Product',
+    slug: 'product',
+  },
+  {
+    id: 7,
+    name: 'Business',
+    slug: 'business',
+  },
+  {
+    id: 8,
+    name: 'Data',
+    slug: 'data',
+  },
+  {
+    id: 9,
+    name: 'DevOps / Sysadmin',
+    slug: 'devops-sysadmin',
+  },
+  {
+    id: 10,
+    name: 'Finance / Legal',
+    slug: 'finance-legal',
+  },
+  {
+    id: 11,
+    name: 'Human Resources',
+    slug: 'human-resources',
+  },
+  {
+    id: 12,
+    name: 'Quality Assurance',
+    slug: 'quality-assurance',
+  },
+  {
+    id: 13,
+    name: 'Writing',
+    slug: 'writing',
+  },
+  {
+    id: 14,
     name: 'Other',
     slug: 'other',
   },

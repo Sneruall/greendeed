61c8b73342980e7c2d5ef4a8ce95ba1267a64b1f
Sneruall <laurens@vr-house.nl>
2022-07-24 16:01:07 +0200

    implement searchbar functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,46 @@ import Header from '../components/Header';
 import JobListing from '../components/JobListing';
 import { Job } from '../types/types';
 import { getJobsFromMongo, getremotiveJobsFromMongo } from '../backend/job/db';
+import { useRouter } from 'next/router';
 
 /*
 Todo:
 - Make sure apis can only be called from our server? (post apis) prevent curl from working,..
 */
 
+let timer: ReturnType<typeof setTimeout>;
+
+// ) => {
+//   setCompanyNameIsLoading(true);
+//   if (timer) {
+//     clearTimeout(timer);
+//   }
+//   timer = setTimeout(() => {
+//     findCompany(value, setRetrievedCompanyData);
+//     setCompanyNameIsLoading(false);
+//   }, 2000);
+// };
+
 const Home: React.FC<{ jobs: [Job] }> = ({ jobs }) => {
+  const router = useRouter();
+
+  const searchInputCallback = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      if (e.target.value.length > 0) {
+        router.push({
+          query: {
+            search: e.target.value,
+          },
+        });
+      } else {
+        router.replace('/', undefined);
+      }
+    }, 300);
+  };
+
   return (
     <div className="">
       <Head>
@@ -20,7 +53,14 @@ const Home: React.FC<{ jobs: [Job] }> = ({ jobs }) => {
       <Header />
       <main className="mx-auto max-w-screen-2xl">
         {/* Searchbar / filtering */}
-        <p className="py-10 text-center">Searchbar</p>
+        search:{' '}
+        <input
+          onChange={(e) => {
+            searchInputCallback(e);
+          }}
+          type="text"
+          className="my-3 border"
+        />
         {/* Listing of jobs */}
         <JobListing jobs={jobs} />
       </main>

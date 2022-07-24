4c8f562c916623e2478a19e280f971a0a78b9220
Sneruall <laurens@vr-house.nl>
2022-07-24 16:05:19 +0200

    add no jobs found

--- components/JobListing.tsx
+++ components/JobListing.tsx
@@ -4,7 +4,7 @@ import JobItem from './JobItem';
 
 //Todo, convert to setup like FormFieldDropdown.tsx (with props at top separate)
 
-const JobListing: React.FC<{ jobs: [Job] }> = ({ jobs }) => {
+const JobListing: React.FC<{ jobs: Job[] }> = ({ jobs }) => {
   const joblist = jobs
     .map((job) => (
       <li className="list-none" key={job.id}>
@@ -13,7 +13,16 @@ const JobListing: React.FC<{ jobs: [Job] }> = ({ jobs }) => {
     ))
     .reverse();
 
-  return <div className="flex flex-col gap-3">{joblist}</div>;
+  return (
+    <div>
+      <div className="flex flex-col gap-3">{joblist}</div>
+      {jobs.length < 1 && (
+        <div className="text-center">
+          <p>No jobs found</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default JobListing;

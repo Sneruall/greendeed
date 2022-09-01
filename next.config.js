d140dbf75e01eb740733a28ab076b2505f2e2241
Sneruall <laurens@vr-house.nl>
2022-09-01 06:43:52 +0200

    setup admin path and cors

--- next.config.js
+++ next.config.js
@@ -6,3 +6,15 @@ module.exports = {
     domains: ['localhost'],
   },
 };
+
+const STUDIO_REWRITE = {
+  source: "/admin/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/admin/:path*"
+      : "/admin/index.html",
+};
+
+module.exports = {
+  rewrites: () => [STUDIO_REWRITE],
+};

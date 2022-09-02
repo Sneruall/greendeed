787d58fb6e0a6dacde09e3a40f99b65633b25ef2
Sneruall <laurens@vr-house.nl>
2022-09-02 08:41:32 +0200

    edit post schema and install react-icons

--- admin/schemas/post.js
+++ admin/schemas/post.js
@@ -1,32 +1,66 @@
+import { HiOutlineDocumentAdd } from '../react-icons/hi';
+
 export default {
   name: 'post',
-  title: 'Post',
+  title: 'Posts',
+  icon: HiOutlineDocumentAdd,
   type: 'document',
   fields: [
     {
       name: 'title',
       title: 'Title',
       type: 'string',
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
+      validation: (Rule) => Rule.required(),
       options: {
         source: 'title',
         maxLength: 96,
       },
     },
     {
+      name: 'excerpt',
+      description: 'Write a short pararaph of this post (For SEO Purposes)',
+      title: 'Excerpt',
+      rows: 5,
+      type: 'text',
+      validation: (Rule) =>
+        Rule.max(160).error(
+          'SEO descriptions are usually better when its below 160'
+        ),
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+      validation: (Rule) => Rule.required(),
+    },
+    {
       name: 'author',
       title: 'Author',
       type: 'reference',
-      to: {type: 'author'},
+      to: { type: 'author' },
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+          description: 'Important for SEO and accessiblity.',
+          options: {
+            isHighlighted: true,
+          },
+        },
+      ],
       options: {
         hotspot: true,
       },
@@ -35,17 +69,13 @@ export default {
       name: 'categories',
       title: 'Categories',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
+      of: [{ type: 'reference', to: { type: 'category' } }],
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'publishedAt',
       title: 'Published at',
       type: 'datetime',
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
     },
   ],
 
@@ -56,10 +86,10 @@ export default {
       media: 'mainImage',
     },
     prepare(selection) {
-      const {author} = selection
+      const { author } = selection;
       return Object.assign({}, selection, {
         subtitle: author && `by ${author}`,
-      })
+      });
     },
   },
-}
+};

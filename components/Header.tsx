b02627c93f12b563a7f3580b435484eb65ca60cf
Sneruall <laurens@vr-house.nl>
2022-09-03 23:54:32 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,12 @@ function Header() {
       <div className="mx-auto flex max-w-screen-2xl justify-end">
         <Link href="/">
           <a className="flex-1">
-            <h1 className="my-auto text-xl font-bold">Metaverse Jobs</h1>
+            <h1 className="my-auto text-xl font-bold">Greendeed</h1>
+          </a>
+        </Link>
+        <Link href="/blog">
+          <a className="flex-1">
+            <h1 className="my-auto text-xl font-bold">Blog</h1>
           </a>
         </Link>
         <Link href="/hiring">

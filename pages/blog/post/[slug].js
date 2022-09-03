78598a523e121461f820549eda347c335a9c1634
Sneruall <laurens@vr-house.nl>
2022-09-03 23:27:08 +0200

    add author and category fetching

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -1,16 +1,51 @@
+// [slug].js
+
+import groq from 'groq';
+import imageUrlBuilder from '@sanity/image-url';
 import client from '../../../client';
 
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source);
+}
+
 const Post = ({ post }) => {
+  const {
+    title = 'Missing title',
+    name = 'Missing name',
+    categories,
+    authorImage,
+  } = post;
   return (
     <article>
-      <h1>{post?.slug?.current}</h1>
+      <h1>{title}</h1>
+      <span>By {name}</span>
+      {categories && (
+        <ul>
+          Posted in
+          {categories.map((category) => (
+            <li key={category}>{category}</li>
+          ))}
+        </ul>
+      )}
+      {authorImage && (
+        <div>
+          <img src={urlFor(authorImage).width(50).url()} />
+        </div>
+      )}
     </article>
   );
 };
 
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  "authorImage": author->image
+}`;
+
 export async function getStaticPaths() {
   const paths = await client.fetch(
-    `*[_type == "post" && defined(slug.current)][].slug.current`
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
   );
 
   return {
@@ -22,17 +57,11 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   // It's important to default the slug so that it doesn't return "undefined"
   const { slug = '' } = context.params;
-  const post = await client.fetch(
-    `
-    *[_type == "post" && slug.current == $slug][0]
-  `,
-    { slug }
-  );
+  const post = await client.fetch(query, { slug });
   return {
     props: {
       post,
     },
   };
 }
-
 export default Post;

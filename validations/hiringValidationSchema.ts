7a9c72b51efe384277870e327493105ecf717ff6
Sneruall <laurens@vr-house.nl>
2022-09-30 16:13:37 +0200

    adjust special chars regex

--- validations/hiringValidationSchema.ts
+++ validations/hiringValidationSchema.ts
@@ -4,7 +4,8 @@ import * as Yup from 'yup';
 // REFACTOR:
 // Make good and consistant error messages as constants, e.g. (.required(requirederror))
 
-const ALPHANUMERIC_AND_SPECIFIC_CHARS = /^[A-zÀ-ÿ\w\-\s\(\)\%\+\,\.\&\#\/]+$/;
+const ALPHANUMERIC_AND_SPECIFIC_CHARS =
+  /^[A-Za-zÀ-ÖØ-öø-ÿ\w\-\s\(\)\%\+\,\.\&\#\/]+$/;
 const NO_BACKWARD_SLASH = /^((?![\\\\]).)*$/;
 const VALID_URL =
   /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;

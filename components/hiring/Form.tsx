77deab4e14f7275408e49f8c1e38599089bba3e0
Sneruall <laurens@vr-house.nl>
2022-11-03 21:14:54 +0100

    styling rich text editors

--- components/hiring/Form.tsx
+++ components/hiring/Form.tsx
@@ -233,29 +233,25 @@ function Form() {
         {/* ////-------/////------////------- STEP 2 -----////------////---////----//// */}
 
         <div className={`${formStep !== 2 && 'hidden'}`}>
-          <h2 className="text-xl">2. Job Description</h2>
+          <div className="flex flex-col gap-5">
+            {/* JOB TITLE */}
+            <FormFieldString
+              id="jobTitle"
+              title="Job Title"
+              placeholder="e.g. Senior Product Manager"
+              register={register}
+              errors={errors.jobTitle}
+            />
 
-          {/* JOB TITLE */}
-          <FormFieldString
-            id="jobTitle"
-            title="Job Title"
-            description="Please specify as single job position like Marketing Manager or Node
-        JS Developer, not a sentence like Looking for PM / Biz Dev / Manager.
-        We know your job is important but please DO NOT WRITE IN FULL CAPS. If
-        posting multiple roles, please create multiple job posts. A job post is
-        limited to a single job. We only allow real jobs, absolutely no MLM-type
-        courses learn how to work online please."
-            register={register}
-            errors={errors.jobTitle}
-          />
-
-          {/* JOB DESCRIPTION  */}
-
-          <h2 className="text-base font-bold">Job description</h2>
-          <RichTextEditor
-            placeholder="Write a good job description..."
-            state={setjobDescriptionHtml}
-          />
+            {/* JOB DESCRIPTION  */}
+            <div>
+              <h2 className="text-base font-bold">Job description</h2>
+              <RichTextEditor
+                placeholder="Write a good job description..."
+                state={setjobDescriptionHtml}
+              />
+            </div>
+          </div>
         </div>
 
         {/* ////-------/////------////------- STEP 3 -----////------////---////----//// */}
--- components/hiring/FormFieldString.tsx
+++ components/hiring/FormFieldString.tsx
@@ -32,7 +32,7 @@ const FormFieldString: React.FC<{
       <label htmlFor={id} className="font-bold text-custom-brown1">
         {title}
       </label>
-      <p className="text-sm text-gray-500 dark:text-gray-400">{description}</p>
+      <p className="text-sm text-gray-500">{description}</p>
       <input
         id={id}
         type={inputType ? inputType : 'text'}
@@ -42,7 +42,7 @@ const FormFieldString: React.FC<{
         step={step}
         {...register(id)}
         onChange={onChangeMethod}
-        className={`my-2 block w-full rounded-lg border border-[#D5D3D3] bg-white py-3 px-4 text-sm text-black shadow-[0_9px_20px_0px_rgba(0,0,0,0.06)] ${
+        className={`my-2 block w-full rounded-lg border border-[#D5D3D3] bg-white py-3 px-4 text-sm text-black shadow-[0_9px_20px_0px_rgba(0,0,0,0.06)] focus:outline-none ${
           errors
             ? 'border-red-300 focus:border-red-500 focus:ring-red-500'
             : 'border-gray-300  focus:border-blue-500 focus:ring-blue-500'

bf9adad8a721f664fca4f88ec6f89bde93f9847d
Sneruall <laurens@vr-house.nl>
2022-11-09 20:23:47 +0100

    started on styling sdg elements in form

--- components/hiring/form-elements/SdgElement.tsx
+++ components/hiring/form-elements/SdgElement.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 import { LocationObject, sdgList } from '../../../types/types';
 import FormFieldBoolCheckbox from '../FormFieldBoolCheckbox';
@@ -10,20 +11,44 @@ type Props = {
 
 function SdgElement({ errors, register, setSdgs }: Props) {
   const optionList = sdgList.map((option) => (
-    <FormFieldBoolCheckbox
-      key={option.code}
-      errors={errors?.sdgs}
-      registerId="sdg"
-      value={option.code}
-      register={register}
-      checkboxText={' ' + option.name}
-      callback={() => {
-        console.log('hoi');
-      }}
-    />
+    <li className="flex" key={option.code}>
+      <div className="">
+        <input
+          value={option.code}
+          type="checkbox"
+          id={option.code}
+          {...register('sdg')}
+          class="peer hidden"
+        />
+        <label
+          htmlFor={option.code}
+          className="inline-flex w-full  cursor-pointer items-center justify-between rounded-lg border border-[#D5D3D3] bg-white p-5 text-gray-500 shadow-[0_9px_20px_0px_rgba(0,0,0,0.06)] hover:bg-gray-50 hover:text-gray-600 peer-checked:border-green-600 peer-checked:text-gray-600 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:peer-checked:text-gray-300"
+        >
+          <div className="block">
+            <div className="inline-flex cursor-pointer self-center border-2 transition duration-200 ease-in-out hover:scale-110">
+              {' '}
+              <Image
+                src={'/images/icons/sdg-icons/' + option.code + '.png'}
+                width={100}
+                height={100}
+                objectFit="contain"
+                layout="intrinsic"
+              />
+            </div>
+            <div className="w-full font-bold text-custom-brown1">
+              {option.name}
+            </div>
+            <div className="w-full text-sm">
+              A JavaScript library for building user interfaces.
+            </div>
+          </div>
+        </label>
+        <div className="text-red-500">{errors?.sdgs?.message}</div>
+      </div>
+    </li>
   ));
 
-  return <div className="grid grid-cols-3">{optionList}</div>;
+  return <ul className="grid w-full gap-6 md:grid-cols-3">{optionList}</ul>;
 }
 
 export default SdgElement;

0ad673e6ca4f4a95cd62b8356b720aec76be9483
Sneruall <laurens@vr-house.nl>
2022-11-25 17:29:47 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 type Props = {};
@@ -5,18 +6,19 @@ type Props = {};
 function Footer({}: Props) {
   return (
     <footer className="z-50 mt-10 bg-custom-brown1 pt-10 text-white">
-      <div className="mx-auto grid max-w-screen-2xl grid-cols-4 px-4 md:px-10 2xl:px-0">
+      <div className="mx-auto grid max-w-screen-2xl grid-cols-2 px-4 py-10 md:px-10 2xl:px-0">
         <div>
-          <h2 className="text-white">Footer</h2>
+          <Link href="/">
+            <a className="my-auto text-4xl font-bold text-white">Greendeed</a>
+          </Link>{' '}
         </div>
         <div>
-          <h2 className="text-white">Footer</h2>
-        </div>
-        <div>
-          <h2 className="text-white">Footer</h2>
-        </div>
-        <div>
-          <h2 className="text-white">Footer</h2>
+          <div>
+            <h2 className="text-white">Footer</h2>
+          </div>
+          <div>
+            <h2 className="text-white">Footer</h2>
+          </div>
         </div>
       </div>
       <div className="pt-20 text-center text-sm">

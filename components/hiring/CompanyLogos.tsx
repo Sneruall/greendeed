ff154c21f88d76d4abe0e37ed8cb2cb8101b10d5
Sneruall <laurens@vr-house.nl>
2022-11-26 09:14:59 +0100

    mobile version logo component

--- components/hiring/CompanyLogos.tsx
+++ components/hiring/CompanyLogos.tsx
@@ -7,11 +7,16 @@ function CompanyLogos() {
       <div className="my-16 flex items-start justify-center gap-10">
         <div className="my-auto w-full border border-custom-grey1"></div>
         <div className="flex-none text-center">
-          <h2 className="heading-wide">Companies already on greendeed</h2>
+          <h2 className="heading-wide">
+            Companies already{' '}
+            <span className="hidden sm:inline">on greendeed</span>
+            <br className="sm:hidden"></br>
+            <span className="sm:hidden">on greendeed</span>
+          </h2>
         </div>
         <div className="my-auto w-full border border-custom-grey1"></div>
       </div>
-      <div className="mx-auto flex max-w-6xl justify-between gap-2 grayscale">
+      <div className="mx-auto flex max-w-6xl flex-col justify-between gap-2 grayscale sm:flex-row">
         <div>
           <Image
             src={'/images/hiring/company1.png'}

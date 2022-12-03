36fa2a2b6b193f8ee8e45fda97533932cb8c3e88
Sneruall <laurens@vr-house.nl>
2022-12-03 16:17:32 +0100

    Update JobListing.tsx

--- components/JobListing.tsx
+++ components/JobListing.tsx
@@ -17,7 +17,7 @@ const JobListing: React.FC<{
   ));
 
   return (
-    <div id="jobs" className="site-margins relative mx-auto mb-10 max-w-7xl">
+    <div id="jobs" className="site-margins relative mx-auto mb-16 max-w-7xl">
       <div
         className="absolute left-1/2 -top-[46px] mb-4 -translate-x-1/2
 -translate-y-1/2 transform text-center"
@@ -31,9 +31,17 @@ const JobListing: React.FC<{
       </div>
       <div className="-mt-8 flex flex-col gap-3">{joblist}</div>
       {jobs?.length < 1 && (
-        <div className="text-center">
-          <p>No sustainable jobs found for '{search}'.</p>
-          <p>Set an alert or sign up for our newsletter to stay tuned!</p>
+        <div
+          className="absolute left-1/2 -top-[46px] mb-4 w-full
+-translate-x-1/2 -translate-y-1/2 transform text-center"
+        >
+          <h2 className="heading-md ">
+            No sustainable jobs found for '{search}'.
+          </h2>
+          <p>
+            Try another search, set an alert or sign up for our newsletter to
+            stay tuned!
+          </p>
         </div>
       )}
     </div>

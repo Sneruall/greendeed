3bd6d0714bceaac4ad28b5dce799224eab3e15a6
Sneruall <laurens@vr-house.nl>
2022-12-25 00:37:50 +0100

    react responsive carousel setup

--- components/job/SimilarJobs.jsx
+++ components/job/SimilarJobs.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+// import 'react-alice-carousel/lib/alice-carousel.css';
 import SimilarJobItem from './SimilarJobItem';
+import ReactDOM from 'react-dom';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const responsive = {
   0: { items: 1 },
@@ -9,6 +11,7 @@ const responsive = {
   1024: { items: 3 },
 };
 
+// todo: not pass currentjob, but simply get the cur job id from the url query param?
 function SimilarJobs({ jobs, currentJob }) {
   const similarJobs = [];
 
@@ -19,13 +22,21 @@ function SimilarJobs({ jobs, currentJob }) {
     similarJobs.push(<SimilarJobItem job={jobs[index]} key={index} />);
   }
 
+  const similarJobList = jobs?.map((job, index) => (
+    <SimilarJobItem job={jobs[index]} key={index} />
+  ));
+
+  console.log('curjob id = ' + currentJob.id);
+
   return (
-    <AliceCarousel
-      mouseTracking
-      items={similarJobs}
-      responsive={responsive}
-      controlsStrategy="alternate"
-    />
+    <Carousel>
+      {jobs
+        ?.filter((job) => job.id !== currentJob.id)
+        .map((job, index) => {
+          // if (currenjobtJob.id === job.id) return;
+          return <SimilarJobItem job={jobs[index]} key={job.id} />;
+        })}
+    </Carousel>
   );
 }
 

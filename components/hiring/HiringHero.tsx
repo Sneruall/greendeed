248a9d6a16eca7b7fb9e0efed36a7919c593d348
Sneruall <laurens@vr-house.nl>
2023-01-20 23:43:35 +0100

    hiring hero

--- components/hiring/HiringHero.tsx
+++ components/hiring/HiringHero.tsx
@@ -5,39 +5,40 @@ import { BiChevronsDown } from 'react-icons/bi';
 
 function HiringHero() {
   return (
-    <div className="site-margins bg-[url('/images/main/bg-topo2x.png')] bg-cover bg-repeat pb-24">
-      <div className="mx-auto max-w-4xl pt-24 sm:pt-36">
+    <div className="site-margins bg-[url('/images/main/bg-topo2x.png')] bg-cover bg-repeat">
+      <div className="mx-auto max-w-5xl pt-24 sm:pt-36">
         <h1 className="heading-2xl mb-6 text-center sm:mb-14">
           Find your next employee
         </h1>
-        <div className="grid lg:grid-cols-2">
+        <div className="grid gap-6 md:grid-cols-2">
           <div>
-            <h2 className="heading-sm">
+            <h2 className="heading-md-omnes">
               Target a large sustainable oriented audience
             </h2>
             <p className="text-main">
               Why posting a job on greendeed, what’s the advantage towards other
               vacancy/recruitment platforms. And also to other green/sustainable
-              platforms. Tell shortly how greendeed works, and why th
+              platforms.
             </p>
-            <h2 className="heading-sm mt-6 sm:mt-8">
-              Find the right candidate sooner because of the SDG’s
+            <h2 className="heading-md-omnes mt-6 sm:mt-8">
+              Find the right candidate sooner
             </h2>
             <p className="text-main">
               Why posting a job on greendeed, what’s the advantage towards other
               vacancy/recruitment platforms. And also to other green/sustainable
-              platforms. Tell shortly how greendeed works, and why the SDG are
-              so
+              platforms. Tell shortly
             </p>
           </div>
-          <div>
-            <Image
-              src={'/images/hiring/interview.png'}
-              width={641}
-              height={480}
-              objectFit="contain"
-              layout="intrinsic"
-            />
+          <div className="flex justify-center">
+            <div className="max-w-sm md:max-w-none">
+              <Image
+                src={'/images/hiring/interview.png'}
+                width={641}
+                height={480}
+                objectFit="contain"
+                layout="intrinsic"
+              />
+            </div>
           </div>
         </div>
 

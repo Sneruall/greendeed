66bc5a901a672dc8c39c26878524ff4fb34cdd44
Sneruall <laurens@vr-house.nl>
2023-01-29 22:51:24 +0100

    fix build error for dynamic Toaster import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,7 @@ import type { AppProps } from 'next/app';
 import 'react-quill/dist/quill.snow.css';
 import ScrollToTop from '../components/ScrollToTop';
 import { Fragment } from 'react';
-import dynamic from 'next/dynamic';
-
-const Toaster = dynamic(
-  () => import('react-hot-toast').then((c) => c.Toaster),
-  {
-    ssr: false,
-  }
-);
+import { Toaster } from 'react-hot-toast'; //todo consider importing dynamic or to specific page only
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

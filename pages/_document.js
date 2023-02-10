b4394d94cc67fc3646d767fad9ed01b0601919c1
Sneruall <laurens@vr-house.nl>
2023-02-10 08:55:43 +0100

    make smooth scroll work again in next13

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html className="scroll-smooth">
+      <Html className="!scroll-smooth">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One"

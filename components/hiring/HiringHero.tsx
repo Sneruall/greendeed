423a3c7621da75dad69e9081128471445cb40b89
Sneruall <laurens@vr-house.nl>
2023-02-10 10:56:48 +0100

    improve design hiring hero

--- components/hiring/HiringHero.tsx
+++ components/hiring/HiringHero.tsx
@@ -7,7 +7,7 @@ function HiringHero() {
   return (
     <div className="site-margins bg-[url('/images/main/bg-topo2x.png')] bg-cover bg-repeat">
       <div className="mx-auto max-w-5xl pt-24 sm:pt-24">
-        <h1 className="heading-2xl mx-auto mb-6 max-w-2xl text-center leading-normal sm:mb-14">
+        <h1 className="heading-2xl mx-auto mb-6 max-w-2xl text-center leading-normal sm:mb-16">
           Build a Better Future with the Right Candidates
         </h1>
         <div className="grid gap-6 md:grid-cols-2">
@@ -28,11 +28,19 @@ function HiringHero() {
               time and resources while still being able to connect with top
               talent in your field.
             </p>
+            <div className="text-center sm:text-left">
+              <Link href="#post-job" className="hover:no-underline">
+                <button className="button-with-icon-4 my-4">
+                  <span className="w-full">Post Job</span>
+                  <BiChevronsDown className="ml-2 h-6 w-6" />
+                </button>
+              </Link>
+            </div>
           </div>
           <div className="flex justify-center">
             <div className="max-w-sm md:max-w-none">
               <Image
-                src={'/images/hiring/interview.png'}
+                src={'/images/hiring/interview-hq.png'}
                 width={641}
                 height={480}
                 objectFit="contain"
@@ -41,15 +49,6 @@ function HiringHero() {
               />
             </div>
           </div>
-        </div>
-
-        <div className="text-center">
-          <Link href="#post-job" className="hover:no-underline">
-            <button className="button-with-icon-4 my-10">
-              <span className="w-full">Post Job</span>
-              <BiChevronsDown className="ml-2 h-6 w-6" />
-            </button>
-          </Link>
         </div>
       </div>
     </div>

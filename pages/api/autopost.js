e37cce6d7a0c52f5a0068387df873f2437223d80
Sneruall <laurens@vr-house.nl>
2023-04-02 11:35:23 +0200

    added auto post in company db

--- pages/api/autopost.js
+++ pages/api/autopost.js
@@ -18,10 +18,42 @@ const handlePost = async (req, res) => {
   if (!data.id) {
     data.id = setJobId();
   }
+  if (data.timestamp === 0) {
+    data.timestamp = Date.now();
+  }
   console.log(data);
   await collection.insertOne(data);
-  res.status(201).json({ message: 'Data inserted successfully in Job DB!' });
+
+  const companyData = {
+    name: data.companyData.name,
+    id: data.companyId,
+    description: data.companyData.description,
+    website: data.companyData.website,
+    sdgs: data.companyData.sdgs,
+    logo: data.companyData.logo,
+  };
+
+  postCompany(companyData);
+
+  res
+    .status(201)
+    .json({ message: 'Data inserted successfully in Job DB and Company DB!' });
 };
+
+async function postCompany(companyData) {
+  const companyResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_HOST}/api/company`,
+    {
+      method: 'POST',
+      body: JSON.stringify(companyData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  const company = await companyResponse.json();
+  console.log(company);
+}
 
 export default async function handler(req, res) {
   switch (req.method) {

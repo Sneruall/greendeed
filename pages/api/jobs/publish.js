0e8ed3dbfc215737043e2fc816035edaaeecc74f
Sneruall <laurens@vr-house.nl>
2023-06-04 17:36:35 +0200

    add timestamp reset functionality

--- pages/api/jobs/publish.js
+++ pages/api/jobs/publish.js
@@ -1,12 +1,10 @@
 /**
- * This is a serverless function that handles a POST request to update a document in a MongoDB
- * collection with a specified ID and set its "published" field to true.
- * @returns The code exports a default asynchronous function that handles HTTP requests. If the request
- * method is POST, it calls the `handlePost` function, which updates a document in a MongoDB collection
- * based on the `id` parameter in the request query. If the update is successful, it sends a JSON
- * response with a success message and a status code of 201. If the request method is not POST, it
+ * This is a function that handles a POST request to update a document in a MongoDB collection with a
+ * new timestamp and set publish to true.
+ * @returns This is a Next.js API route that handles a POST request to update a document in a MongoDB
+ * collection. If the request is successful, a JSON response with a message is returned. If the request
+ * method is not POST, a 405 status code is returned.
  */
-
 import clientPromise from '../../lib/mongodb';
 
 const getCollection = async () => {
@@ -15,16 +13,24 @@ const getCollection = async () => {
   return db.collection(process.env.MONGODB_COLLECTION);
 };
 
+// Function to reset timestamp
+const resetTimestamp = () => {
+  return Date.now();
+};
+
 const handlePost = async (req, res) => {
   const { id } = req.query;
   const collection = await getCollection();
   const filter = { id: id };
-  const updateDoc = { $set: { published: true } };
+  // Use the resetTimestamp function to set the new timestamp
+  const updateDoc = { $set: { published: true, timestamp: resetTimestamp() } };
   const result = await collection.updateOne(filter, updateDoc);
   console.log(
     `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
   );
-  res.status(201).json({ message: 'Job published successfully!' });
+  res
+    .status(201)
+    .json({ message: 'Job published and timestamp updated successfully!' });
 };
 
 export default async function handler(req, res) {

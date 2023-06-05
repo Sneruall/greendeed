b137e2cf0013cda6d690570358af762e545dceb1
Sneruall <laurens@vr-house.nl>
2023-06-05 08:05:06 +0200

    update tweet templates to include link to greendeed

--- pages/api/jobs/publish.js
+++ pages/api/jobs/publish.js
@@ -30,21 +30,21 @@ const resetTimestamp = () => {
 const postTweet = async (job) => {
   // Array of tweet templates
   const tweetTemplates = [
-    `🎯 New Job: ${job.jobTitle} at ${job.companyData.name}! Help us build a greener future. #Greendeed #GreenJobs #CleanTechJobs`,
+    `🎯 New Job: ${job.jobTitle} at ${job.companyData.name}! Help us build a greener future. Apply here: https://greendeed.io/#jobs #Greendeed #GreenJobs #CleanTechJobs`,
     `🌱 Want to contribute to a sustainable future? ${job.companyData.name} is now hiring for ${job.jobTitle}. Check it out: https://greendeed.io/#jobs #SustainableJobs #Greendeed`,
-    `💚 Hot job alert: ${job.jobTitle} at ${job.companyData.name}! Apply now and join us in creating a greener future. #Greendeed #GreenJobs #EcoCareers`,
-    `🍃 Exciting new job: ${job.jobTitle} at ${job.companyData.name}! Make your next career move count. #Greendeed #EcoFriendlyJobs #SustainableWork`,
+    `💚 Hot job alert: ${job.jobTitle} at ${job.companyData.name}! Apply now and join us in creating a greener future. https://greendeed.io/#jobs #Greendeed #GreenJobs #EcoCareers`,
+    `🍃 Exciting new job: ${job.jobTitle} at ${job.companyData.name}! Make your next career move count. Apply: https://greendeed.io/#jobs #Greendeed #EcoFriendlyJobs #SustainableWork`,
     `💪 Join the green movement! ${job.companyData.name} is now hiring a ${job.jobTitle}. More info: https://greendeed.io/#jobs #Greendeed #GreenCareers #Sustainability`,
-    `⭐ New opportunity: ${job.jobTitle} at ${job.companyData.name}! Help us make a difference. #Greendeed #EcoJobs #GreenJobs`,
-    `🌏 Contribute to our planet’s future! Apply for the new ${job.jobTitle} position at ${job.companyData.name}. #Greendeed #EcoCareers #Sustainability`,
-    `🌿 New job alert: ${job.jobTitle} at ${job.companyData.name}! Come and join the green revolution. #Greendeed #GreenJobs #SustainableWork`,
-    `🔥 Hot off the press: ${job.companyData.name} is hiring for the role of ${job.jobTitle}. #Greendeed #EcoFriendlyJobs #GreenCareers`,
-    `🌎 Make your work matter! Check out this new ${job.jobTitle} role at ${job.companyData.name}. #Greendeed #EcoJobs #Sustainability`,
-    `💫 New green job alert: ${job.jobTitle} at ${job.companyData.name}. Let's make a difference together! #Greendeed #SustainableJobs #EcoCareers`,
-    `✨ ${job.companyData.name} is now hiring a ${job.jobTitle}! Be a part of the change. #Greendeed #GreenJobs #SustainableWork`,
-    `🌈 Dream job alert: ${job.jobTitle} at ${job.companyData.name}. Apply now and help us create a more sustainable world. #Greendeed #EcoFriendlyJobs #EcoCareers`,
-    `⚡ Exciting career opportunity: ${job.jobTitle} at ${job.companyData.name}! Let's work towards a greener future. #Greendeed #GreenJobs #SustainableWork`,
-    `🌠 Join the green force! New ${job.jobTitle} position open at ${job.companyData.name}. #Greendeed #EcoJobs #Sustainability`,
+    `⭐ New opportunity: ${job.jobTitle} at ${job.companyData.name}! Help us make a difference. Apply here: https://greendeed.io/#jobs #Greendeed #EcoJobs #GreenJobs`,
+    `🌏 Contribute to our planet’s future! Apply for the new ${job.jobTitle} position at ${job.companyData.name}. More info: https://greendeed.io/#jobs #Greendeed #EcoCareers #Sustainability`,
+    `🌿 New job alert: ${job.jobTitle} at ${job.companyData.name}! Come and join the green revolution. Apply now: https://greendeed.io/#jobs #Greendeed #GreenJobs #SustainableWork`,
+    `🔥 Hot off the press: ${job.companyData.name} is hiring for the role of ${job.jobTitle}. Check it out: https://greendeed.io/#jobs #Greendeed #EcoFriendlyJobs #GreenCareers`,
+    `🌎 Make your work matter! Check out this new ${job.jobTitle} role at ${job.companyData.name}. Apply here: https://greendeed.io/#jobs #Greendeed #EcoJobs #Sustainability`,
+    `💫 New green job alert: ${job.jobTitle} at ${job.companyData.name}. Let's make a difference together! Apply now: https://greendeed.io/#jobs #Greendeed #SustainableJobs #EcoCareers`,
+    `✨ ${job.companyData.name} is now hiring a ${job.jobTitle}! Be a part of the change. Apply here: https://greendeed.io/#jobs #Greendeed #GreenJobs #SustainableWork`,
+    `🌈 Dream job alert: ${job.jobTitle} at ${job.companyData.name}. Apply now and help us create a more sustainable world. https://greendeed.io/#jobs #Greendeed #EcoFriendlyJobs #EcoCareers`,
+    `⚡ Exciting career opportunity: ${job.jobTitle} at ${job.companyData.name}! Let's work towards a greener future. More info: https://greendeed.io/#jobs #Greendeed #GreenJobs #SustainableWork`,
+    `🌠 Join the green force! New ${job.jobTitle} position open at ${job.companyData.name}. Apply now: https://greendeed.io/#jobs #Greendeed #EcoJobs #Sustainability`,
   ];
 
   // Choose a random tweet

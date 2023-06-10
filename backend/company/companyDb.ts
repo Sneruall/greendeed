8ee9b2e153e5d59d80e6194727da4d836eee97db
Sneruall <laurens@vr-house.nl>
2023-06-10 17:06:09 +0200

    Revert "add error handling to company db"
    This reverts commit 89328939f7a4d30bf87457a1826298d9599c8f67.

--- backend/company/companyDb.ts
+++ backend/company/companyDb.ts
@@ -2,44 +2,39 @@ import clientPromise from '../../lib/mongodb';
 import { Company } from '../../types/types';
 
 export const getCompanyFromMongo = async (queryId: string) => {
-  try {
-    const client = await clientPromise;
-    const db = client.db();
+  /* Using the MongoDB driver to connect to the database and retrieve a job from the database. */
+  const client = await clientPromise;
 
-    let collection: string;
-    if (process.env.MONGODB_COMPANY_COLLECTION) {
-      collection = process.env.MONGODB_COMPANY_COLLECTION;
-    } else {
-      throw new Error('Please add your Mongo URI to .env.local');
-    }
+  const db = client.db();
 
-    const company = (await db
-      .collection(collection)
-      .findOne({ id: queryId })) as unknown as Company;
-
-    return company;
-  } catch (err) {
-    console.error(`Error getting company from Mongo: ${err}`);
-    throw err; // Re-throwing the error to be handled by the caller
+  let collection: string;
+  if (process.env.MONGODB_COMPANY_COLLECTION) {
+    collection = process.env.MONGODB_COMPANY_COLLECTION;
+  } else {
+    throw new Error('Please add your Mongo URI to .env.local');
   }
+
+  const company = (await db
+    .collection(collection)
+    .findOne({ id: queryId })) as unknown as Company;
+
+  return company;
 };
 
 export const getCompaniesFromMongo = async () => {
-  try {
-    const client = await clientPromise;
-    const db = client.db();
+  /* Using the MongoDB driver to connect to the database and retrieve a job from the database. */
+  const client = await clientPromise;
 
-    let collection: string;
-    if (process.env.MONGODB_COMPANY_COLLECTION) {
-      collection = process.env.MONGODB_COMPANY_COLLECTION;
-    } else {
-      throw new Error('Please add your Mongo URI to .env.local');
-    }
+  const db = client.db();
 
-    const companies = await db.collection(collection).find({}).toArray();
-    return companies;
-  } catch (err) {
-    console.error(`Error getting companies from Mongo: ${err}`);
-    throw err; // Re-throwing the error to be handled by the caller
+  let collection: string;
+  if (process.env.MONGODB_COMPANY_COLLECTION) {
+    collection = process.env.MONGODB_COMPANY_COLLECTION;
+  } else {
+    throw new Error('Please add your Mongo URI to .env.local');
   }
+
+  const companies = await db.collection(collection).find({}).toArray();
+
+  return companies;
 };

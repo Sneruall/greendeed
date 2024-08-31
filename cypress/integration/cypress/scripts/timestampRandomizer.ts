2075a2b3df47a47ba39082a58aafc3c504da834d
Sneruall <laurens@vr-house.nl>
2024-08-31 11:14:20 +0200

    update to use hours instead of days

--- cypress/integration/cypress/scripts/timestampRandomizer.ts
+++ cypress/integration/cypress/scripts/timestampRandomizer.ts
@@ -1,28 +1,24 @@
-// timestampRandomizer.ts
-
 /**
- * Generate a random timestamp in milliseconds from 1970, based on a given range.
- * @param {number} daysRange - The number of days to randomize.
- *                             If 0, 1, or omitted, it returns the current time.
- *                             If greater than 1, it returns the current time minus up to 'daysRange' days.
+ * Generate a random timestamp in milliseconds from 1970, based on a given hour range.
+ * @param {number} hoursRange - The number of hours to randomize.
+ *                              If 0, 1, or omitted, it returns the current time.
+ *                              If greater than 1, it returns the current time minus up to 'hoursRange' hours.
  * @returns {number} - The timestamp in milliseconds.
  */
-export function getRandomTimestamp(daysRange?: number): number {
+export function getRandomTimestamp(hoursRange?: number): number {
   // Calculate the current time in milliseconds
   const currentTime = Date.now();
 
-  // If daysRange is 0, 1, or omitted, return the current time
-  if (!daysRange || daysRange === 1) {
+  // If hoursRange is 0, 1, or omitted, return the current time
+  if (!hoursRange || hoursRange === 1) {
     return currentTime;
   }
 
-  // Randomize the number of days to subtract (0 to daysRange - 1)
-  const randomTime = Math.random() * daysRange;
+  // Randomize the number of hours to subtract (0 to hoursRange - 1)
+  const randomTime = Math.random() * hoursRange;
 
-  // Calculate the timestamp by subtracting the number of random days in milliseconds
-  const randomTimestamp = Math.floor(
-    currentTime - randomTime * 24 * 60 * 60 * 1000
-  ); // 24 hours in milliseconds
+  // Calculate the timestamp by subtracting the number of random hours in milliseconds
+  const randomTimestamp = Math.floor(currentTime - randomTime * 60 * 60 * 1000); // 1 hour in milliseconds
 
   return randomTimestamp;
 }
